fb4539638eff33e1f55edaaf108079c2e1b344dc
FaizJD030 <165897256+FaizJD030@users.noreply.github.com>
2025-10-22 13:43:48 +0100

    ADJST-1348 Get Prisoner Image for MiniProfile (#8)

---
+++ server/routes/prisonerImageRoutes.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express'
+import path from 'path'
+import PrisonerSearchService from '../services/prisonerSearchService'
+
+const placeHolderImage = path.join(process.cwd(), '/assets/images/prisoner-profile-image.png')
+
+export default class PrisonerImageRoutes {
+  constructor(private readonly prisonerService: PrisonerSearchService) {}
+
+  getImage = async (req: Request, res: Response): Promise<void> => {
+    const { nomsId } = req.params
+
+    return this.prisonerService
+      .getPrisonerImage(nomsId, res.locals.user)
+      .then(data => {
+        res.set('Cache-control', 'private, max-age=86400')
+        res.removeHeader('pragma')
+        res.type('image/jpeg')
+        data.pipe(res)
+      })
+      .catch(_error => {
+        res.sendFile(placeHolderImage)
+      })
+  }
+}
