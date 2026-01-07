64b46f9187275e535b74b07baa95f414e3a5a5b8
dave-virden <151018159+dave-virden@users.noreply.github.com>
2026-01-07 13:24:37 +0000

    Allow default image to be displayed in dev if no image is present. (#23)

--- server/routes/prisonerImageRoutes.ts
+++ server/routes/prisonerImageRoutes.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express'
 import path from 'path'
 import PrisonerSearchService from '../services/prisonerSearchService'
 
-const placeHolderImage = path.join(process.cwd(), '/assets/images/prisoner-profile-image.png')
+const placeHolderImage = path.join(process.cwd(), '/dist/assets/images/prisoner-profile-image.png')
 
 export default class PrisonerImageRoutes {
   constructor(private readonly prisonerService: PrisonerSearchService) {}
