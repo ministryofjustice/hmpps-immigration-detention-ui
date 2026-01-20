ce8e8d831cf5d08262a0126aed4102716db2f283
carloveo-moj <144013256+carloveo-moj@users.noreply.github.com>
2026-01-20 15:10:55 +0000

    RASS-1799 align CSP with other uis (#50)

--- server/middleware/setUpWebSecurity.ts
+++ server/middleware/setUpWebSecurity.ts
@@ -13,21 +13,32 @@ export default function setUpWebSecurity(): Router {
     res.locals.cspNonce = crypto.randomBytes(16).toString('hex')
     next()
   })
+
+  const scriptSrc = ["'self'", (_req: Request, res: Response) => `'nonce-${res.locals.cspNonce}'`]
+  const styleSrc = ["'self'", (_req: Request, res: Response) => `'nonce-${res.locals.cspNonce}'`]
+  const fontSrc = ["'self'"]
+  const imgSrc = ["'self'", 'data:']
+  const formAction = [`'self' ${config.apis.hmppsAuth.externalUrl} ${config.services.courtCasesReleaseDates.url}`]
+  const connectSrc = ["'self' https://northeurope-0.in.applicationinsights.azure.com https://js.monitor.azure.com"]
+
+  if (config.apis.frontendComponents.enabled) {
+    scriptSrc.push(config.apis.frontendComponents.url)
+    styleSrc.push(config.apis.frontendComponents.url)
+    imgSrc.push(config.apis.frontendComponents.url)
+    fontSrc.push(config.apis.frontendComponents.url)
+  }
+
   router.use(
     helmet({
       contentSecurityPolicy: {
         directives: {
           defaultSrc: ["'self'"],
-          // This nonce allows us to use scripts with the use of the `cspNonce` local, e.g (in a Nunjucks template):
-          // <script nonce="{{ cspNonce }}">
-          // or
-          // <link href="http://example.com/" rel="stylesheet" nonce="{{ cspNonce }}">
-          // This ensures only scripts we trust are loaded, and not anything injected into the
-          // page by an attacker.
-          scriptSrc: ["'self'", (_req: Request, res: Response) => `'nonce-${res.locals.cspNonce}'`],
-          styleSrc: ["'self'", (_req: Request, res: Response) => `'nonce-${res.locals.cspNonce}'`],
-          fontSrc: ["'self'"],
-          formAction: [`'self' ${config.apis.hmppsAuth.externalUrl} ${config.services.courtCasesReleaseDates.url}`],
+          scriptSrc,
+          styleSrc,
+          fontSrc,
+          imgSrc,
+          formAction,
+          connectSrc,
           ...(config.production ? {} : { upgradeInsecureRequests: null }),
         },
       },
