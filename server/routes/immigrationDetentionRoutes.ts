74920139c9fe2f242b139c77b43290bafa3567bc
Md Hasan Rahman <Hassan.rahman@justice.gov.uk>
2026-05-07 16:59:41 +0100

    RASS-2125 immigration detention uuids added to audit event

--- server/routes/immigrationDetentionRoutes.ts
+++ server/routes/immigrationDetentionRoutes.ts
@@ -89,6 +89,7 @@ export default class ImmigrationDetentionRoutes {
         subjectId: nomsId,
         details: {
           time: Date.now(),
+          immigrationDetentionUuids: immigrationDetentionList.map(immigrationDetention => immigrationDetention.immigrationDetentionUuid),
         },
       })
 
