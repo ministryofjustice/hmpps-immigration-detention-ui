9eea9a9e3da6bfc908d908a17d6cc33ad8abc5ba
Yasin Mustafa <yasin.mustafa@gmail.com>
2026-06-26 14:21:59 +0100

    Merge with typescript template (#121)

---
+++ integration_tests/mockApis/frontEndComponentsApi.ts
@@ -0,0 +1,34 @@
+import type { SuperAgentRequest } from 'superagent'
+import { stubFor } from './wiremock'
+
+export default {
+  stubPing: (httpStatus = 200): SuperAgentRequest =>
+    stubFor({
+      request: {
+        method: 'GET',
+        urlPath: '/frontend-components/ping',
+      },
+      response: {
+        status: httpStatus,
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        jsonBody: { status: httpStatus === 200 ? 'UP' : 'DOWN' },
+      },
+    }),
+  stubComponents: (httpStatus = 200): SuperAgentRequest =>
+    stubFor({
+      request: {
+        method: 'GET',
+        urlPath: '/frontend-components/components',
+        queryParameters: {
+          component: { equalTo: 'header' },
+        },
+      },
+      response: {
+        status: httpStatus,
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        jsonBody: {
+          header: { html: '' },
+        },
+      },
+    }),
+}
